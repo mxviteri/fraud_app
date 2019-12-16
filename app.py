7f9421bb92032afbf4558c190104cb2e48d83acb
Matt Viteri <mxviteri@gmail.com>
2019-12-15 21:31:58 -0600

    changing the way variables are used

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ app = Flask(__name__,
 
 CORS(app)
 
-test = None
-model = None
 maps = {'card4': [('visa', 3), ('mastercard', 2), ('american express', 0), ('discover', 1)], 'ProductCD': [('W', 4), ('H', 1), ('C', 0), ('S', 3), ('R', 2)], 'card6': [('credit', 1), ('debit', 2), ('debit or credit', 3), ('charge card', 0)], 'P_emaildomain': [('gmail.com', 16), ('outlook.com', 35), ('yahoo.com', 53), ('mail.com', 29), ('anonymous.com', 1), ('hotmail.com', 19), ('verizon.net', 48), ('aol.com', 2), ('me.com', 30), ('comcast.net', 9), ('optonline.net', 34), ('cox.net', 10), ('charter.net', 8), ('rocketmail.com', 42), ('prodigy.net.mx', 37), ('embarqmail.com', 12), ('icloud.com', 23), ('live.com.mx', 26), ('gmail', 15), ('live.com', 25), ('att.net', 3), ('juno.com', 24), ('ymail.com', 58), ('sbcglobal.net', 43), ('bellsouth.net', 4), ('msn.com', 31), ('q.com', 40), ('yahoo.com.mx', 54), ('centurylink.net', 6), ('servicios-ta.com', 45), ('earthlink.net', 11), ('hotmail.es', 21), ('cfl.rr.com', 7), ('roadrunner.com', 41), ('netzero.net', 33), ('gmx.de', 17), ('suddenlink.net', 46), ('frontiernet.net', 14), ('windstream.net', 50), ('frontier.com', 13), ('outlook.es', 36), ('mac.com', 28), ('netzero.com', 32), ('aim.com', 0), ('web.de', 49), ('twc.com', 47), ('cableone.net', 5), ('yahoo.fr', 57), ('yahoo.de', 55), ('yahoo.es', 56), ('sc.rr.com', 44), ('ptd.net', 39), ('live.fr', 27), ('yahoo.co.uk', 52), ('hotmail.fr', 22), ('hotmail.de', 20), ('hotmail.co.uk', 18), ('protonmail.com', 38), ('yahoo.co.jp', 51)]}
 
 # serving the static react frontend
@@ -21,16 +19,10 @@ def serve(path):
 
 @app.route('/api/fraud', methods=['post'])
 def prediction():
-    global test
-    global model
-
-    if test is None:
-        test = pd.read_csv('./data/scaleddownsampletest.csv')
-
-    if model is None:
-        with open('./data/final_model.pkl', 'rb') as f:
-            model = pickle.load(f)
-
+    test = pd.read_csv('./data/scaleddownsampletest.csv')
+    model = None
+    with open('./data/final_model.pkl', 'rb') as f:
+        model = pickle.load(f)
 
     body = request.json
 
